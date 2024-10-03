e842a9aca1c2d966550d2a28fa707834fb4f2b0e
ErynVarghese <erynvarghese@gmail.com>
2024-10-03 04:56:11 -0400

    Added Department Repo with content

---
+++ SMS/Repositories/DepartmentRepo.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SMS.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SMS.Repositories
+{
+    public class DepartmentRepo : ICommon<Department>
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sms"].ConnectionString);
+        SqlCommand cmd = null;
+
+        public List<Department> GetAll()
+        {
+            List<Department> deptlist = new List<Department>();
+
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("proc_dept", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Mode", 0);
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    Department dept = new Department();
+                    dept.DeptId = sdr.GetInt32(0);
+                    dept.DeptName = sdr.GetString(1);
+                    deptlist.Add(dept);
+                }
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw ex.InnerException;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return deptlist;
+        }
+
+        public string Create(Department obj)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                con.Open();
+
+                cmd = new SqlCommand("proc_dept", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DeptName", obj.DeptName);
+                cmd.Parameters.AddWithValue("@Mode", 2);
+
+                int status = cmd.ExecuteNonQuery();
+
+                if (status <= 0)
+                {
+                    result = "Success";
+                }
+
+                else
+                {
+                    result = "Fail";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw ex.InnerException;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return result;
+        }
+
+        public string Update(Department obj)
+        {
+            string result = string.Empty;
+
+            try
+            {
+
+                cmd = new SqlCommand("proc_dept", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DeptId", obj.DeptId);
+                cmd.Parameters.AddWithValue("@DeptName", obj.DeptName);
+                cmd.Parameters.AddWithValue("@Mode", 3);
+
+                int status = cmd.ExecuteNonQuery();
+
+                if (status <= 0)
+                {
+                    result = "Success";
+                }
+
+                else
+                {
+                    result = "Fail";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw ex.InnerException;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return result;
+
+        }
+
+
+        public string Delete(int Id)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                con.Open();
+
+                cmd = new SqlCommand("proc_dept", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DeptId", Id);
+                cmd.Parameters.AddWithValue("@Mode", 4);
+
+                int status = cmd.ExecuteNonQuery();
+
+                if (status <= 0)
+                {
+                    result = "Success";
+                }
+
+                else
+                {
+                    result = "Fail";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw ex.InnerException;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return result;
+        }
+
+
+        public Department GetById(int Id = 0)
+        {
+            Department obj = new Department();
+
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("proc_dept", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DeptId", Id);
+                cmd.Parameters.AddWithValue("@Mode", 1);
+
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    obj.DeptId = sdr.GetInt32(0);
+                    obj.DeptName = sdr.GetString(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw ex.InnerException;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return obj;
+        }
+    }
+}
