b8f3841062ef60acda3c6578d9d773c4ddcd9a53
ErynVarghese <erynvarghese@gmail.com>
2024-10-03 03:34:37 -0400

    Added Student Repo

---
+++ SMS/Repositories/ICommon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS.Repositories
+{
+    interface ICommon<T>
+    {
+        List<T> GetAll();
+
+        string Create(T obj);
+
+        string Update(T obj);
+
+        string Delete(int Id);
+
+        T GetById(int Id);
+    }
+}
